751b1909ab1a445cec41d53ffd1d6c690c6dada0
hweijing <jh13290900214@163.com>
2024-06-04 01:39:09 +0800

    2024.6.4

---
+++ vue/19_单文件组件/Student.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>学生姓名：{{ name }}</h1>
+    <h1>学生年龄：{{ age }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "zhangsan",
+      age: 18,
+    };
+  },
+};
+</script>
+
+<style>
+</style>
